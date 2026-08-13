fe7a93d5de2226778242424b27c30f20a4bbf0c2
jeanmm03 <132017545+jeanmm03@users.noreply.github.com>
2026-08-12 21:19:16 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // PREENCHA APENAS ESTES DOIS CAMPOS.
 // Supabase > Project Settings > API
 
-export const SUPABASE_URL = "COLE_AQUI_SUA_PROJECT_URL";
-export const SUPABASE_ANON_KEY = "COLE_AQUI_SUA_ANON_KEY";
+export const SUPABASE_URL = "https://kpzlsbfffenwdxkesxym.supabase.co/rest/v1/";
+export const SUPABASE_ANON_KEY = "sb_publishable_8dnJamrSI-l_wvH_WQ-NiQ_zVXmd1h7";
