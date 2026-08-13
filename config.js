f2412848a04cf36183abce5089feb3fc73da7d8f
jeanmm03 <132017545+jeanmm03@users.noreply.github.com>
2026-08-12 21:39:35 -0300

    Refactor Supabase URL and ANON_KEY exports

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// PREENCHA APENAS ESTES DOIS CAMPOS.
-// Supabase > Project Settings > API
+export const SUPABASE_URL =
+  "https://kpzlsbfffenwdxkesxym.supabase.co";
 
-export const SUPABASE_URL = "https://kpzlsbfffenwdxkesxym.supabase.co/rest/v1/";
-export const SUPABASE_ANON_KEY = "sb_publishable_8dnJamrSI-l_wvH_WQ-NiQ_zVXmd1h7";
+export const SUPABASE_ANON_KEY =
+  "sb_publishable_8dnJamrSI-l_wvH_WQ-NiQ_zVXmd1h7";
